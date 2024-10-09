ad53374b1368633fc5b12d14655865286eed5942
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-09 14:27:01 -0500

    Add files via upload

---
+++ In-between card game/index.js
@@ -0,0 +1,20 @@
+//The to do list
+
+/* 
+    -To start off I must first create an object that has the key-value pairs of every card that is usable
+
+    -I will then grab the references to the neccesary html components so that i can attach functionality to them
+
+    -Then I must create a function that activates when the button on the webpage is pressed to deal the player 1 card at random 
+    
+    (I could also just pre-draw the cards as the player presses the button so that I wont have to run a fucntion multiple times)
+
+    -If that card is an ace, prompt the player to declare the ace high or low value
+
+    -Then deal the next card to the player and prompt them to declare whether or not the next card they draw will be a value that is in between their current 2 cards
+
+    -The player wins if the next card is in between and they lose if it is not
+
+    -Create a function that clears the board after the game has ended and return the button on screen for the player to get dealt another card if they choose
+
+*/
