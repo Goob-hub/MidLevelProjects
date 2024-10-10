797b88aa8fcd6d68fd152f843a79b8b86cc44104
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-10 12:38:17 -0500

    Set up some basic functionality for project
    - So far I have created game logic for dealing cards and keeping track of the specific logic necessary to deal the correct amount of cards if the player is dealt an ace first or not dealt an ace first.
    
    - Also added some basic styling to the site and changed some html formatting

--- In-between card game/index.js
+++ In-between card game/index.js
@@ -1,20 +1,117 @@
 //The to do list
 
 /* 
-    -To start off I must first create an object that has the key-value pairs of every card that is usable
+    So far I have created game logic for dealing cards and keeping track of the speicic logic neccesary to deal the correct amount of cards if the player is dealt an ace first or not dealt an ace first
 
-    -I will then grab the references to the neccesary html components so that i can attach functionality to them
+    Make sure the card added to selected cards is an object that contains its value and key, and to make sure that the aces are of correct value
 
-    -Then I must create a function that activates when the button on the webpage is pressed to deal the player 1 card at random 
-    
-    (I could also just pre-draw the cards as the player presses the button so that I wont have to run a fucntion multiple times)
+    Next we must make sure the player is able to declare high or low on the ace if that is the first card they are dealt
 
-    -If that card is an ace, prompt the player to declare the ace high or low value
+    We also have to finish functionality for the player to decide whether or not the next card they are dealt will be in between their current card set
 
-    -Then deal the next card to the player and prompt them to declare whether or not the next card they draw will be a value that is in between their current 2 cards
+    Add winning or losing result feedback on screen for player
 
-    -The player wins if the next card is in between and they lose if it is not
+    Next figure out a money system for the player, possibly start them with $20
 
-    -Create a function that clears the board after the game has ended and return the button on screen for the player to get dealt another card if they choose
+    if they lose a bet on the card being in between they lose $5
 
+    if they win they gain $5
+
+    the goal is to reach $50
+
+    if they reach 50 they win but if they hit 0 they lose
 */
+
+const dealBtn = document.querySelector(".deal-btn");
+const highBtn = document.querySelector(".high-btn");
+const lowBtn = document.querySelector(".low-btn");
+const inBetweenBtn = document.querySelector(".in-between");
+const notInBetweenBtn = document.querySelector(".not-in-between");
+const playingArea = document.querySelector(".playing-area");
+const cardContainers = document.querySelectorAll(".card-container");
+
+const cardData = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": {"High": 14, "Low": 1}
+}
+const cards = Object.keys(cardData);
+
+let selectedCards = [];
+
+let curCard = selectedCards.length;
+
+function getRandomCard(){
+    let randomCard = cards[randNum(cards.length)];
+    //let randomCard = "A";
+    selectedCards.push(randomCard);
+}
+
+function randNum(max){
+    return Math.floor(Math.random() * max);
+}
+
+function gameLogic(){
+    
+    if(curCard === 2){
+        inBetweenBtn.classList.remove("hide");
+        notInBetweenBtn.classList.remove("hide");
+    }
+
+    if(curCard === 1){
+        addCardToScreen();
+        getRandomCard();
+        curCard++;
+    }
+
+    if(selectedCards[curCard] === "A" && curCard === 0){
+        highBtn.classList.remove("hide");
+        lowBtn.classList.remove("hide");
+        getRandomCard();
+        addCardToScreen();
+        curCard++;
+    } else {
+        for (let i = 0; i < 2; i++) {
+            getRandomCard();
+            addCardToScreen();
+            curCard++;
+        }
+        inBetweenBtn.classList.remove("hide");
+        notInBetweenBtn.classList.remove("hide");
+    }
+
+    console.log(curCard);
+
+}
+
+function addCardToScreen(){
+    
+    let cardContainer = cardContainers[curCard];
+    let image = cardContainer.children[0];
+    
+    image.classList.add("card-image");
+    image.src = `./imgs/${selectedCards[curCard]}.png`;
+
+        
+    
+}
+
+dealBtn.addEventListener("click", (e) => {
+    gameLogic();
+    dealBtn.classList.add("hide");
+});
+
+
+
+
+
