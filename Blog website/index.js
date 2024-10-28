5d1c341f4df67eff0e2c1502948d3bfff0c7a0d4
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-28 17:25:57 -0500

    Set up foundation for project
    Got basic boilerplate and files set up

---
+++ Blog website/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log("Site live on port 3000");
+})
